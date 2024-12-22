77d731db741f51346dc59375c8921e16c31f74ec
Inigo Burrell <inigoburrell@gmail.com>
2024-12-22 11:15:21 +0000

    quotations

--- test2.py
+++ test2.py
@@ -31,7 +31,7 @@ class PubSub(object):
         self.logger.info("{0}, {1} - {2}".format(userdata, msg.topic, msg.payload))
         msg_json = json.loads(msg.payload)
         if msg_json['message'] == "textScroll":
-            os.system(f'sudo /home/inigo/panel/rpi-rgb-led-matrix/examples-api-use/scrolling-text-example -f ../../fonts/ibmfonts/bdf/ic8x16u.bdf --led-rows=64 --led-cols=64 {msg_json['content']}')
+            os.system(f'sudo /home/inigo/panel/rpi-rgb-led-matrix/examples-api-use/scrolling-text-example -f ../../fonts/ibmfonts/bdf/ic8x16u.bdf --led-rows=64 --led-cols=64 {msg_json["content"]}')
         #os.system('sudo /home/inigo/panel/rpi-rgb-led-matrix/examples-api-use/minimal-example --led-cols=64 --led-rows=64 --led-gpio-mapping=regular-pi1')
 
     def __on_log(self, client, userdata, level, buf):
