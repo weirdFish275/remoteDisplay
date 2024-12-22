ba78d0ed8f335be5b1c98be4a3d415ad99be208d
Inigo Burrell <inigoburrell@gmail.com>
2024-12-22 22:53:45 +0000

    fixed clock

--- test2.py
+++ test2.py
@@ -51,7 +51,7 @@ class PubSub(object):
             subprocess.Popen([f'sudo killall text-example'], shell=True)
             subprocess.Popen([f'sudo killall clock'], shell=True)
             sleep(0.5)
-            display_process = subprocess.Popen([f'sudo /home/inigo/panel/rpi-rgb-led-matrix/examples-api-use/clock -f ../fonts/ibmfonts/bdf/ic8x16u.bdf --led-rows=64 --led-cols=64 --led-gpio-mapping=regular-pi1 -y {msg_json["y"]} -x {msg_json["x"]} -C {msg_json["textColour"][0]},{msg_json["textColour"][1]},{msg_json["textColour"][2]} -B {msg_json["bgColour"][0]},{msg_json["bgColour"][1]},{msg_json["bgColour"][2]}'], shell=True)
+            display_process = subprocess.Popen([f'sudo /home/inigo/panel/rpi-rgb-led-matrix/examples-api-use/clock -f ../fonts/ibmfonts/bdf/ic8x16u.bdf --led-rows=64 --led-cols=64 --led-gpio-mapping=regular-pi1'], shell=True)
 
     def __on_log(self, client, userdata, level, buf):
         self.logger.debug("{0}, {1}, {2}, {3}".format(client, userdata, level, buf))
