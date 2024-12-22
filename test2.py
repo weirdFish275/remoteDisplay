f933898403fa3eee0be603c28139d4f5dff8c78d
Inigo Burrell <inigoburrell@gmail.com>
2024-12-22 16:31:55 +0000

    trying to kill

--- test2.py
+++ test2.py
@@ -10,6 +10,7 @@ import signal
 import logging
 logging.basicConfig(level=logging.INFO)
 global pid
+global display_process
 
 # Refactored original source - https://github.com/mariocannistra/python-paho-mqtt-for-aws-iot
 
@@ -34,7 +35,7 @@ class PubSub(object):
         msg_json = json.loads(msg.payload)
         if msg_json['message'] == "textScroll":
             try:
-                os.kill(pid, signal.SIGKILL)
+                display_process.send_signal(signal.SIGINT)
             except UnboundLocalError:
                 pass
             display_process = subprocess.Popen([f'sudo /home/inigo/panel/rpi-rgb-led-matrix/examples-api-use/scrolling-text-example -f ../fonts/ibmfonts/bdf/ic8x16u.bdf --led-rows=64 --led-cols=64 --led-gpio-mapping=regular-pi1 {msg_json["content"]} -y {msg_json["y"]} -x {msg_json["x"]}'], shell=True)
